84c631b0e390865a188ed8fa5a8bf2278527fa41
vince-vibin <99386370+vince-vibin@users.noreply.github.com>
2022-03-02 20:27:08 +0100

    moved gamble to games

--- cogs/games.py
+++ cogs/games.py
@@ -88,5 +88,35 @@ class Games(commands.Cog):
             embed.add_field(name="Guesses so far:", value=hangman_instance.get_guess_string(), inline=False)
             await ctx.send(embed=embed)
     
+    @commands.command(description="Get a random number from 1-100. But Whuay??",brief="Get a random number from 1-100. But Whuay??")
+    async def roll(self, ctx):
+        n = random.randrange(1,101)
+
+        colour=0xC14EB2
+        embed = discord.Embed(colour=colour)
+        embed.add_field(name="Congratulations you got a:", value=n, inline=False)
+        embed.set_footer(text="Now move on with your live and get hobbys.")
+        await ctx.send(embed=embed)
+
+    @commands.command(description="Roll a dice cause you dont have any hobbys.",brief="Roll a dice cause you dont have any hobbys.")
+    async def dice(self, ctx):
+        n = random.randrange(1, 6)
+        
+        colour=0xC14EB2
+        embed = discord.Embed(colour=colour)
+        embed.add_field(name="You rolled a:", value=n, inline=False)
+        embed.set_footer(text="Now move on with your live and get hobbys.")
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases=['coin'], description="Just flip a coin (i dont know whuay you would).",brief="Just flip a coin (i dont know whuay you would).")
+    async def coinflip(self, ctx):
+        n = random.choice(('Heads', 'Tails'))
+        
+        colour=0xC14EB2
+        embed = discord.Embed(colour=colour)
+        embed.add_field(name="You got:", value=n, inline=False)
+        embed.set_footer(text="Now move on with your live and get hobbys.")
+        await ctx.send(embed=embed)
+    
 def setup(bot):
     bot.add_cog(Games(bot))
