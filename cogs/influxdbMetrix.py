e15455236404d99717eab132753efb99316d7f71
vince-vibin <99386370+vince-vibin@users.noreply.github.com>
2022-03-22 19:05:07 +0100

    added sytem metric to influxDB

--- cogs/influxdbMetrix.py
+++ cogs/influxdbMetrix.py
@@ -1,7 +1,11 @@
-import discord
 from discord.ext import commands, tasks
+import psutil
 
 from influx.influxdbExport import sendingServers
+from influx.influxdbExport import sendingSYS
+
+# This file is used to get Metrix for InfluxDB about uptime number of servers and more
+# it s in a cog to be initalized when the bot is starting up
 
 class InfluxMetrix(commands.Cog):
     def __init__(self, bot):
@@ -11,11 +15,27 @@ class InfluxMetrix(commands.Cog):
     @tasks.loop(minutes=1)
     async def exportServer(self):
         serversNum = len(self.bot.guilds)
-        serverList = self.bot.guilds
-
-        print(serverList, serversNum)
 
         sendingServers(serversNum)
+
+    @tasks.loop(minutes=1)
+    async def getSysData():
+        ram = psutil.virtual_memory()
+        disk = psutil.disk_usage("/")
+        i = 0
+
+        cpuPerc = [psutil.cpu_percent(), "cpuUsed%", "cpu"]
+        ramUsed = [ram.percent, "ramUsed%", "ram"]
+        diskUsedPerc = [disk.percent, "diskUsed%", "disk"]
+        
+        send = [cpuPerc, ramUsed, diskUsedPerc]
+
+        while i < len(send): #looping throught send array
+            sendingSYS(send[i])
+            i = i + 1
+
+    getSysData.start()
+
 
 def setup(bot):
     bot.add_cog(InfluxMetrix(bot))
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+from wsgiref.simple_server import sys_version
 from discord.ext import commands,tasks
 import os
 from settings import *
 import discord
-
 
 bot = commands.Bot(command_prefix="$")
 bot.remove_command("help")
 
 @bot.event
 async def on_ready():
+    print("Python Version: ", sys_version)
     activity = discord.Game(name="sleeping")
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     print("YeeeeeBot online")
